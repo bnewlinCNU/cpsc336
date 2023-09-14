b3066fdc2d7ea42c191ef64f40b4a0112553d99f
bnewlinCNU <143013236+bnewlinCNU@users.noreply.github.com>
2023-09-14 18:11:27 -0400

    left should be done

--- left.sh
+++ left.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo student
+echo start
+
 echo LEFT > /etc/hostname
+sed -i 's/FA21-ICA4/LEFT/' /etc/hosts
+
 echo auto enp0s8 >> /etc/network/interfaces
 echo iface enp0s8 inet static >> /etc/network/interfaces
 echo address 192.168.1.2 >> /etc/network/interfaces
 echo netmask 255.255.255.0 >> /etc/network/interfaces
 
-sed -i 's/FA21-ICA4/LEFT/' /etc/hosts
-echo done!
+echo ping -c 1 192.168.1.3 >> /etc/rc.local
+chmod +x /etc/rc.local
+
+reboot
