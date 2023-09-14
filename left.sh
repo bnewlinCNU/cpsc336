e692cb22ea27223fa3d7bd5474861ecbd5e9241d
bnewlinCNU <143013236+bnewlinCNU@users.noreply.github.com>
2023-09-14 18:45:58 -0400

    Update left.sh

--- left.sh
+++ left.sh
@@ -10,7 +10,13 @@ echo iface enp0s8 inet static >> /etc/network/interfaces
 echo address 192.168.1.2 >> /etc/network/interfaces
 echo netmask 255.255.255.0 >> /etc/network/interfaces
 
-sed -i 's/# bits./ping -c 1 192.168.1.3 >> /etc/rc.local/' /etc/hostsecho
+
+
+sed -i 's/# bits./ping -c 1 192.168.1.3 >> /etc/rc.local/'
 chmod +x /etc/rc.local
 
+passwd
+echo student
+echo .
+echo .
 reboot
