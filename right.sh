37950767d0b52fbdc1aefeb44d6cd4b24f571990
bnewlinCNU <143013236+bnewlinCNU@users.noreply.github.com>
2023-09-14 18:13:10 -0400

    Create right.sh

---
+++ right.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo start
+
+echo RIGHT > /etc/hostname
+sed -i 's/FA21-ICA4/RIGHT/' /etc/hosts
+
+echo auto enp0s8 >> /etc/network/interfaces
+echo iface enp0s8 inet static >> /etc/network/interfaces
+echo address 192.168.1.3 >> /etc/network/interfaces
+echo netmask 255.255.255.0 >> /etc/network/interfaces
+
+echo ping -c 1 192.168.1.2 >> /etc/rc.local
+chmod +x /etc/rc.local
+
+reboot
